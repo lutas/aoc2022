0cb241a92058524fca6eb8e9286872b10aeb39c0
Phil Lutas <phil@lutas.co.uk>
2022-12-08 19:00:39 +0000

    Day 4 Part 2 completed

--- Day4/index.js
+++ Day4/index.js
@@ -29,12 +29,35 @@ const isRangeContained = (src, dest) => {
     }    
 }
 
+const overlaps = (src, dest) => {
+
+    // test the lower bound of range2
+    if (isRangeContained(src, { lower: dest.lower, upper: dest.lower })) {
+        return true;
+    }
+    
+    // test the upper bound of range2
+    if (isRangeContained(src, { lower: dest.upper, upper: dest.upper })) {
+        return true;
+    }
+
+    // test the lower bound of range1
+    if (isRangeContained(dest, { lower: src.lower, upper: src.lower })) {
+        return true;
+    }
+
+    // test the upper bound of range1
+    if (isRangeContained(dest, { lower: src.upper, upper: src.upper})) {
+        return true;
+    }
+}
+
 const data = fs.readFileSync('input.txt', 'utf-8').split('\n');
 let score = 0;
 data.forEach(line => {
     [ range1, range2 ] = parseTextLine(line);
 
-    if (isRangeContained(range1, range2)) {
+    if (overlaps(range1, range2)) {
         ++score;
     }
 });
