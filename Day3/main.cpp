0e06ad7cf059f567997bcc11447013b50cc4f320
Phil Lutas <phil@lutas.co.uk>
2022-12-08 17:26:51 +0000

    Day 3 Part 2 complete

--- Day3/main.cpp
+++ Day3/main.cpp
@@ -5,6 +5,7 @@
 
 char GetCommonLetter(const std::string& pack) {
 
+    // drop to cstring for ease of iteration
     const char* szPack = pack.c_str();
     auto compartmentLength  = pack.length() / 2;
 
@@ -23,9 +24,32 @@ char GetCommonLetter(const std::string& pack) {
     throw std::invalid_argument("Failed to find common letter");
 }
 
-int ScorePriority(const char c) {
+char GetCommonLetter(const std::string& elf1, const std::string& elf2, const std::string& elf3) {
 
-    std::cout << "Scoring letter " << c;
+    // drop to cstrings for ease of iteration
+    const char* szElf1 = elf1.c_str();
+    const char* szElf2 = elf2.c_str();
+    const char* szElf3 = elf3.c_str();
+
+    for (const char* e1 = szElf1; *e1 != '\0'; ++e1) {
+        for (const char* e2 = szElf2; *e2 != '\0'; ++e2) {
+
+            // is the char common between the first two elves?
+            if (*e1 == *e2) {
+                // if the char also in elf 3?
+                for (const char* e3 = szElf3; *e3 != '\0'; ++e3) {
+                    if (*e1 == *e3) {
+                        return *e1;
+                    }
+                }
+            }
+        }
+    }
+
+    throw std::invalid_argument("Failed to find a common letter in group of elves");
+}
+
+int ScorePriority(const char c) {
 
     const int A = 0x41;
     const int Z = 0x5A;
@@ -49,9 +73,9 @@ int ScorePriority(const char c) {
     throw std::invalid_argument("Invalid char input found");
 }
 
-int main() {
+int day1(const char* filePath) {
 
-    std::ifstream f("input.txt", std::fstream::in);
+    std::ifstream f(filePath, std::fstream::in);
 
     std::string strPack;
     int line = 0;
@@ -68,5 +92,37 @@ int main() {
         ++line;
     }
 
-    std::cout << "Total score = " << totalScore << std::endl;
+    return totalScore;
 };
+
+int day2(const char* filePath) {
+    std::ifstream f(filePath, std::fstream::in);
+
+    std::string elf[3];
+    int group = 0;
+    int totalScore = 0;
+    while (std::getline(f, elf[0])) {
+
+        // assume that there are always groups of 3 elves 
+        // (i.e. number of lines in input file is a multiple of 3)
+        std::getline(f, elf[1]);
+        std::getline(f, elf[2]);
+        
+        const char commonLetter = GetCommonLetter(elf[0], elf[1], elf[2]);
+        const int score = ScorePriority(commonLetter);
+        std::cout << "Group " << group + 1 << " = " << commonLetter 
+                  << " (" << score << ") pts" << std::endl;
+
+        totalScore += score;
+
+        ++group;
+    }
+
+    return totalScore;
+}
+
+int main() {
+    int totalScore = day2("input.txt");
+    
+    std::cout << "Total score = " << totalScore << std::endl;
+}
